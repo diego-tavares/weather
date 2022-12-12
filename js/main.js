2a206fe20f9b80580b105e8902ee03d4b609fba7
Carlos Diego Tavares <102318991+diego-tavares@users.noreply.github.com>
2022-12-11 23:39:30 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-const inputText = document.getElementById('inputText')
-const searchButton = document.getElementById('searchButton')
-const cidade = document.getElementById('cidade')
-const pais = document.getElementById('pais')
-const tempo = document.getElementById('tempo')
-const tipo = document.getElementById('tipo')
-const temperatura = document.getElementById('temperatura')
+const inputText = document.getElementById('inputText');
+const searchButton = document.getElementById('searchButton');
+const cidade = document.getElementById('cidade');
+const pais = document.getElementById('pais');
+const tempo = document.getElementById('tempo');
+const tipo = document.getElementById('tipo');
+const temperatura = document.getElementById('temperatura');
 
 searchButton.addEventListener('click', () => {
     console.log('ok')
+    cidade.innerHTML = "OI"
+    pais.innerHTML = "Teste"
 })
+
