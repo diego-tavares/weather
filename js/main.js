558e3a6062c5f8650fa4c2787dca0e838ff74908
Carlos Diego Tavares <102318991+diego-tavares@users.noreply.github.com>
2022-12-11 23:21:47 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const inputText = document.getElementById('inputText')
+const searchButton = document.getElementById('searchButton')
 const cidade = document.getElementById('cidade')
 const pais = document.getElementById('pais')
 const tempo = document.getElementById('tempo')
 const tipo = document.getElementById('tipo')
 const temperatura = document.getElementById('temperatura')
+
+searchButton.addEventListener('click', () => {
+    console.log('ok')
+})
